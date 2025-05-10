42a43b82216798e2a1abfdba8e00dff0f309c7ab
Eris M <erismathison@gmail.com>
2025-05-11 01:23:10 +1000

    Initial Commit

---
+++ Calculator/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,25 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace Calculator.ViewModels;
+
+public partial class MainWindowViewModel : ViewModelBase
+{
+    public string Text
+    {
+        get => _text;
+        set
+        {
+            SetProperty(ref _text, value);
+            IsValid = IsValidValue();
+        }
+    }
+    
+    [ObservableProperty]
+    private bool _isValid = false;
+    private string _text = "";
+
+    private bool IsValidValue()
+    {
+        return float.TryParse(_text, out _);
+    }
+}
