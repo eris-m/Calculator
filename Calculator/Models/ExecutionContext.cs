d4aaa017cfad6dfad623fbc812547f52254268a9
Eris M <erismathison@gmail.com>
2025-05-20 23:10:40 +1000

    Added more functions.

--- Calculator/Models/ExecutionContext.cs
+++ Calculator/Models/ExecutionContext.cs
@@ -23,7 +23,7 @@ public record struct FunctionDescription(int ExpectedArguments, Function Fun);
 
 public class FunctionRegistry
 {
-    private Dictionary<string, FunctionDescription> _functions;
+    private Dictionary<string, FunctionDescription> _functions = [];
 
     public FunctionRegistry()
     {
@@ -42,13 +42,44 @@ public class FunctionRegistry
         _functions.Add(name, fn);
     }
 
+    public void DefineFunction(string name, Func<double, double> fn)
+    {
+        DefineFunction(name, new FunctionDescription(1, xs => fn(xs[0])));
+    }
+
+    public void DefineFunction(string name, Func<double, double, double> fn)
+    {
+        DefineFunction(name, new FunctionDescription(2, xs => fn(xs[0], xs[1])));
+    }
+
     private void DefaultFunctions()
     {
         // maybe set default length.
         _functions = [];
-
-        _functions["sqrt"] = new FunctionDescription(1, (xs) => double.Sqrt(xs[0]));
-        _functions["floor"] = new FunctionDescription(1, (xs) => double.Floor(xs[0]));
-        _functions["ceil"] = new FunctionDescription(1, (xs) => double.Ceiling(xs[0]));
+        
+        #region Arithmetic functions.
+        DefineFunction("pow", double.Pow);
+        DefineFunction("sqrt", double.Sqrt);
+        DefineFunction("root", (x, n) => double.RootN(x, (int)n));
+        #endregion
+    
+        #region Trig functions.
+        // regular
+        DefineFunction("sin", double.Sin);
+        DefineFunction("cos", double.Cos);
+        DefineFunction("tan", double.Tan);
+        
+        // inverse
+        DefineFunction("asin", double.Asin);
+        DefineFunction("acos", double.Acos);
+        DefineFunction("atan", double.Atan);
+        #endregion
+        
+        #region Misc functions.
+        DefineFunction("min", double.Min);
+        DefineFunction("max", double.Max);
+        DefineFunction("floor", double.Floor);
+        DefineFunction("ceil", double.Ceiling);
+        #endregion
     }
 }
